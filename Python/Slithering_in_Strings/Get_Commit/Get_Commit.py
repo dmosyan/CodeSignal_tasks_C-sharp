c03708551e48ec433f105945f2ff2e0098dfde6d
David Mosyan <dmosyan@gmail.com>
2019-09-13 01:29:32 +0400

    Add files via upload

---
+++ Python/Slithering_in_Strings/Get_Commit/Get_Commit.py
@@ -0,0 +1,4 @@
+def getCommit(commit):
+    return commit.lstrip("0?+!")
+  #The lstrip() method returns a copy of the string with leading characters
+  # removed (based on the string argument passed)
