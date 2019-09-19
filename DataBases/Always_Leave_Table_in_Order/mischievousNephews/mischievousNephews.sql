03e9e5ac7813c5c89eee14b81489563a418e845f
David Mosyan <dmosyan@gmail.com>
2019-09-19 15:04:29 +0400

    Add files via upload

---
+++ DataBases/Always_Leave_Table_in_Order/mischievousNephews/mischievousNephews.sql
@@ -0,0 +1,8 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE mischievousNephews()
+BEGIN
+
+SELECT MOD( DAYOFWEEK(mischief_date) + 5, 7 ) AS weekday, mischief_date, author, title FROM mischief
+ORDER BY weekday, Field(author, 'Huey', 'Dewey' ,'Louie'), mischief_date, title;
+
+END
