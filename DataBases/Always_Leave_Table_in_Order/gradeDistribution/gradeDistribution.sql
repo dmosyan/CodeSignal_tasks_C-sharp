ac376b7b247400091e3a7cc2529d572983093a58
David Mosyan <dmosyan@gmail.com>
2019-09-19 15:04:46 +0400

    Add files via upload

---
+++ DataBases/Always_Leave_Table_in_Order/gradeDistribution/gradeDistribution.sql
@@ -0,0 +1,9 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE gradeDistribution()
+BEGIN
+
+SELECT Name, ID FROM Grades
+WHERE (Midterm1 / 2 + Midterm2 / 2) < Final
+ORDER BY SUBSTRING(Name, 1, 3), ID;
+
+END
