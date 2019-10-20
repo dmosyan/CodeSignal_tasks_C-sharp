46f377d44245e7f209627ee5f0e930600d37254f
David Mosyan <dmosyan@gmail.com>
2019-10-20 23:32:00 +0400

    Add files via upload

---
+++ The Core/waterTubes.cs
@@ -0,0 +1,18 @@
+int waterTubes(int[] water, int[] flowPerMinute) 
+{
+    int result = 0;
+
+    for (int i = 0; i < water.Length; i++) 
+    {
+        int minutes = water[i] / flowPerMinute[i];
+        if (water[i] % flowPerMinute[i] != 0) 
+        {
+            minutes++;
+        }
+        if (result < minutes) 
+        {
+            result = minutes;
+        }
+    }
+    return result;
+}
