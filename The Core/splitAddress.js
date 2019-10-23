5880537163e5e3f989b0036a9085072cbe4529da
David Mosyan <dmosyan@gmail.com>
2019-10-23 23:57:14 +0400

    Add files via upload

---
+++ The Core/splitAddress.js
@@ -0,0 +1,16 @@
+function splitAddress(address) 
+{
+    var result = address.split("://")
+    var protocol = result[0];
+    result = result[1].split(".com");
+    var domain = result[0];
+    result = result[1].split("/");
+
+
+    if(result[1]){
+        return [protocol,domain,result[1]]
+    }
+
+    return [protocol,domain]
+}
+
