ee9a36961fc119ea4bdb4d4e14f84c12a69d9f17
David Mosyan <dmosyan@gmail.com>
2019-10-30 13:22:30 +0400

    Add files via upload

---
+++ DataBases/WHEN Was It the CASE/placOfInterests.sql
@@ -0,0 +1,11 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE placesOfInterest()
+BEGIN
+    SELECT country, SUM(CASE WHEN leisure_activity_type = 'Adventure park' THEN number_of_places ELSE 0 END) as                             adventure_park,
+    SUM(CASE WHEN leisure_activity_type = 'Golf' THEN number_of_places ELSE 0 END) as                                       golf,
+    SUM(CASE WHEN leisure_activity_type = 'River cruise' THEN number_of_places ELSE 0 END) as                               river_cruise,
+    SUM(CASE WHEN leisure_activity_type = 'Kart racing' THEN number_of_places ELSE 0 END) as                               kart_racing FROM countryActivities
+    GROUP BY country
+    ORDER BY country ASC;
+	
+END
